7135be1efaffcc247d07fa7684dcaf9188fc779d
gayan-de-silva <gayanchamath072@gmail.com>
2021-09-21 20:02:13 +0530

    t1

--- hello/views.py
+++ hello/views.py
@@ -5,7 +5,7 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    return HttpResponse({'a': 5})
+    return HttpResponse("Ssssssss")
     # return render(request, "index.html")
 
 
